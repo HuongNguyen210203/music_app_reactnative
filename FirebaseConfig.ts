4b28a8b21f3615a9406a51092138c18609a3f263
huongnguyen <huongthugaquay883@gmail.com>
2024-10-10 16:48:06 +0700

    Add FirebaseConfig.ts

---
+++ FirebaseConfig.ts
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"; // Import Firebase Storage
+
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA0_if_J3ng3_Yb7VWWtwpOr2aAXUcM-Ic",
+    authDomain: "reanatauth.firebaseapp.com",
+    projectId: "reanatauth",
+    storageBucket: "reanatauth.appspot.com", // Default storage bucket
+    messagingSenderId: "550340552858",
+    appId: "1:550340552858:web:7d36557a0e188d57c0d723",
+    measurementId: "G-RSFTCW7HNC"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const FIREBASE_AUTH = getAuth(firebaseApp);
+const FIREBASE_DB = getFirestore(firebaseApp);
+const FIREBASE_STORAGE = getStorage(firebaseApp, "gs://reanatauth.appspot.com"); // Initialize Firebase Storage with custom bucket
+
+// Set persistence
+setPersistence(FIREBASE_AUTH, browserLocalPersistence)
+    .then(() => {
+        console.log("Persistence set to browserLocalPersistence.");
+    })
+    .catch((error) => {
+        console.error("Error setting persistence: ", error);
+    });
+
+// Export the initialized Firebase app and services
+export { firebaseApp, FIREBASE_AUTH, FIREBASE_DB, FIREBASE_STORAGE };
